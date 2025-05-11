bec4b83b048db914dbad165401b67caf8987bcda
Ramesh Maharjan <ramesh.anahcolus@gmail.com>
2025-05-11 15:48:52 +0545

    svelte learning first phase

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	let { children } = $props();
+</script>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/card">card</a>
+</nav>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  let count = $state(0);
+</script>
+<h1>Welcome to SvelteKit</h1>
+<button onclick={() => count++}>Count: {count}</button>
