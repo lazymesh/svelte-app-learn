e0c35a0cdad61ee1140fb6fabb00e52ca4f741aa
Ramesh Maharjan <ramesh.anahcolus@gmail.com>
2025-06-06 19:06:26 +0545

    about and calendar page added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,8 @@
 <nav>
 	<a href="/">home</a>
 	<a href="/card">card</a>
+	<a href="/about">about</a>
+	<a href="/calendar">calendar</a>
 </nav>
 
 {@render children()}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+  let count = $state(0);
+</script>
+<h1>Welcome to About page</h1>
