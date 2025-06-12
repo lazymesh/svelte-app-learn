80f4428a5bc2e4a2d50f418f06052f2243e1cf2b
Ramesh Maharjan <ramesh.anahcolus@gmail.com>
2025-06-12 09:20:27 +0545

    learning svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <script>
+  import Nested from './Nested.svelte';
+  let property = " and import word is sent from parent";
   let count = $state(0);
   let str = $state("");
+  let totalStr = $derived(str.length);
 </script>
 <h1>Welcome to Svelte TRYINGS</h1>
 <h2>this is me learning and applying svelte stuffs where i have tried developing nepali calendar</h2>
 <button onclick={() => count++}>Count: {count}</button>
 <button onclick={() => str = str+"x"}>Add x: {str}</button>
+{#if totalStr > 0}
+<p>{totalStr} x added</p>
+{/if}
+<Nested property={property}></Nested>
---
+++ src/routes/Nested.svelte
@@ -0,0 +1,14 @@
+<script>
+    let { property } = $props();
+    let info = "this is nested <strong>svelte component</strong>";
+</script>
+
+<p>{@html info} {property}</p>
+
+<style>
+p {
+    color:rgb(235, 10, 10);
+    font-weight: 2rem;
+    font-size: 2rem;
+}
+</style>
